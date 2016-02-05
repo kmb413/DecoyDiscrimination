886c9cd43ac5421d9f8b8537e3037556d47d5edf
Kristin Blacklock - Khare group <svensken@rutgers.edu>
2016-02-05 18:49:02 -0500

    Updated ConvertToPDBandSubmitMinimizationJobs.py to create a new working directory inside the old one if the silent files have already been extracted before. New directory's name is based on datetime (YearMonthDay.HourMinuteSecond).

--- scripts/ConvertToPDBandSubmitMinimizationJobs.py
+++ scripts/ConvertToPDBandSubmitMinimizationJobs.py
@@ -1,8 +1,4 @@
-import os, sys, getopt, math
-#import pytraj as pt
-#from rosetta import *
-#from toolbox import pdb_from_rcsb
-#init()
+import os, sys, getopt, math, time
 
 PATH_TO_DECOYDISC = "/scratch/kmb413/RealDecoyDisc/DecoyDiscrimination"
 ROSETTA_BIN = PATH_TO_DECOYDISC+"/extract_pdbs.static.linuxgccrelease"
@@ -66,12 +62,12 @@ def main(argv):
     else:
         print("Each job will contain a maximum of %i minimization jobs(s)." % num_mins_per_job)
 
-    ########################################################################
+    #####################################
     ### Create PDBs from Silent Files ###
-    ########################################################################
+    #####################################
     print( "\t===== Creating PDBs from Silent File =====" )
     
-    ## Set 1 or Set 2? ##
+    ## Decoy Set 1 or Set 2? ##
     if decoy_set_index == 1:
         decoy_set = "decoys.set1.init"
         file_ending = "1000.out"
@@ -79,57 +75,76 @@ def main(argv):
         decoy_set = "decoys.set2.init"
         file_ending = "retag.nocartbump.out"
 
+    ## Has the silent file been extracted before? If so, make a new working directory in the old directory so we do not overwrite. ##
+    dt = ''
     if not os.path.exists( "%s/%s/%s/" % ( PATH_TO_DECOYDISC, decoy_set, input_pdb )):
         os.mkdir( "%s/%s/%s/" % ( PATH_TO_DECOYDISC, decoy_set, input_pdb ) )
+        os.chdir( "%s/%s/%s/" % ( PATH_TO_DECOYDISC, decoy_set, input_pdb ) )
+        
+        ## Copy Native to working directory. ##
+        os.system("cp %s/Natives/%s_0001.clean.pdb ." % (PATH_TO_DECOYDISC, input_pdb ))
+
     else:
-        print( "ERROR: %s/%s/%s EXISTS." % ( PATH_TO_DECOYDISC, decoy_set, input_pdb ) )
-        sys.exit()
+        print( "WARNING: %s/%s/%s EXISTS." % ( PATH_TO_DECOYDISC, decoy_set, input_pdb ) )
+        dt = time.strftime("%y%m%d.%H%M%S")
+        print( "Making new working directory for this minimization run...%s/%s/%s/%s" % (PATH_TO_DECOYDISC, decoy_set, input_pdb, dt) )
+        os.mkdir( "%s/%s/%s/%s/" % ( PATH_TO_DECOYDISC, decoy_set, input_pdb, dt ) )
+        os.chdir( "%s/%s/%s/%s/" % ( PATH_TO_DECOYDISC, decoy_set, input_pdb, dt ) )
+        #sys.exit()
     
-    os.chdir( "%s/%s/%s/" % ( PATH_TO_DECOYDISC, decoy_set, input_pdb ) )
+    if dt == '':
+        os.system( "%s -database %s -in:file:silent %s/%s/%s.%s" % (ROSETTA_BIN, ROSETTA_DB, PATH_TO_DECOYDISC, decoy_set, input_pdb, file_ending ) )
+        decoys = os.popen('ls empty*.pdb').readlines()  ## List of pdbs ["<pdb1>\n", "<pdb2>\n", ...]
+        
+        ##################################
+        ### Remove Hydrogens from PDBs ###
+        ##################################
+        no_h_decoys = []
+        for pdb in decoys:
+            os.system("sed '/     H  /d' %s > NoH_%s" % ( pdb.rstrip(), pdb.rstrip() ) )
+            no_h_decoys.append('NoH_%s' % pdb.rstrip())
 
-    ## Copy Native to working directory.
-    os.system("cp %s/Natives/%s_0001.clean.pdb ." % (PATH_TO_DECOYDISC, input_pdb ))
+        ##################################################
+        ### Create RST7 and PARM7 files from NoH_PDBs. ###
+        ##################################################
+        with open('tleap.in','w') as tfile:
+            tfile.write("source leaprc.ff14SBonlysc\n")
+            tfile.write("m = loadpdb @@@.pdb\n")
+            tfile.write("set default pbradii mbondi3\n")
+            tfile.write("saveamberparm m @@@.parm7 @@@.rst7\n")
+            tfile.write("quit")
+
+        for pdb in no_h_decoys:
+            os.system('sed "s/@@@/%s/g" tleap.in > %s.tleap.in' % ( pdb.rstrip('.pdb\n'), pdb.rstrip('.pdb\n') ) )
+            os.system('tleap -f %s.tleap.in' % ( pdb.rstrip('.pdb\n') ) )
+
+    else:
+        no_h_decoys = os.popen('ls %s/%s/%s/NoH_*.pdb' % (PATH_TO_DECOYDISC, decoy_set, input_pdb) ).readlines()
+        for i in range(len(no_h_decoys)):
+            no_h_decoys[i] = no_h_decoys[i].rstrip()
     
-    os.system( "%s -database %s -in:file:silent %s/%s/%s.%s" % (ROSETTA_BIN, ROSETTA_DB, PATH_TO_DECOYDISC, decoy_set, input_pdb, file_ending ) )
-    
-    decoys = os.popen('ls *.pdb').readlines()  ## List of pdbs ["<pdb1>\n", "<pdb2>\n", ...]
-
-    ########################################################################
-    ### Remove Hydrogens from PDBs ###
-    ########################################################################
-
-    #no_h_decoys = []
-    #mask = '!@H='
-    #for decoy in decoys:
-    #    traj = pt.iterload( decoy.rstrip() )
-    #    traj[mask].save( 'NoH_'+decoy.rstrip() )
-    #    no_h_decoys.append('NoH_%s' % decoy.rstrip())
-    
-    no_h_decoys = []
-    for pdb in decoys:
-        os.system("sed '/     H  /d' %s > NoH_%s" % ( pdb.rstrip(), pdb.rstrip() ) )
-        no_h_decoys.append('NoH_%s' % pdb.rstrip())
-
-    ########################################################################
-    ### Create RST7 and PARM7 files from NoH_PDBs. Minimize using Sander ###
-    ########################################################################
-
-    with open('tleap.in','w') as tfile:
-        tfile.write("source leaprc.ff14SBonlysc\n")
-        tfile.write("m = loadpdb @@@.pdb\n")
-        tfile.write("set default pbradii mbondi3\n")
-        tfile.write("saveamberparm m @@@.parm7 @@@.rst7\n")
-        tfile.write("quit")
-
+    ######################################
+    ### Generate Minimization Commands ###
+    ######################################
     minimization_commands = []
     for pdb in no_h_decoys:
-        os.system('sed "s/@@@/%s/g" tleap.in > %s.tleap.in' % ( pdb.rstrip('.pdb\n'), pdb.rstrip('.pdb\n') ) )
-        os.system('tleap -f %s.tleap.in' % ( pdb.rstrip('.pdb\n') ) )
-        minimization_commands.append('sander -i %s -o %s.out -p %s.parm7 -c %s.rst7 -r min_%s.rst7 -ref %s.rst7\n' % ( min_script, pdb.rstrip('.pdb\n'), pdb.rstrip('.pdb\n'), pdb.rstrip('.pdb\n'), pdb.rstrip('.pdb\n'), pdb.rstrip('.pdb\n') ) )
+
+        if dt != '':
+            min_rst7 = "/".join(pdb.split('/')[0:-1]) + "/" + dt + "/min_"+pdb.split('/')[-1].rstrip('.pdb\n')+'.rst7'
+        else:
+            min_rst7 = "min_" + pdb.rstrip('.pdb\n') + ".rst7"
+
+        if dt != '':
+            min_out = "/".join(pdb.split('/')[0:-1]) + "/" + dt + "/min_"+pdb.split('/')[-1].rstrip('.pdb\n')+'.out'
+        else:
+            min_out = "min_" + pdb.rstrip('.pdb\n') + ".out"
+        
+        minimization_commands.append('sander -i %s -o %s -p %s.parm7 -c %s.rst7 -r %s -ref %s.rst7\n' % ( min_script, min_out, pdb.rstrip('.pdb\n'), pdb.rstrip('.pdb\n'), min_rst7, pdb.rstrip('.pdb\n') ) )
 
 
     #################################
     ##### Submit to Slurm Queue #####
+    #################################
     if num_total_jobs > -1:
         num_mins = int( math.ceil( float(len(minimization_commands) )/float(num_total_jobs) ) )
         minimization_chunks = chunks(minimization_commands, num_mins)
@@ -146,10 +161,8 @@ def main(argv):
             minfile.write("#!/bin/bash\n#SBATCH -n 1\n#SBATCH -c 1\n#SBATCH -o MinimizationScript%i.out\n#SBATCH --job-name=%sM%i\n\nsource %s/amber.sh\n\n" % ( file_count, input_pdb, file_count, AMBER_HOME ) )
             for minimization_command in minchunk:
                 minfile.write(minimization_command)
-        #os.system('sbatch MinimizationScript%i.sh' % file_count)
+        os.system('sbatch MinimizationScript%i.sh' % file_count)
     ################################
-
-
 
 if __name__ == "__main__":
     main(sys.argv[1:])
