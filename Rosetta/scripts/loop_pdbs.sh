b165ac669e64105f47e8dd0a5be5b09e1574db53
Aliza Rubenstein F12 Khare aliza.rubenstein@rutgers.edu <arubenstein@gaann5.iqb.rutgers.edu>
2016-02-10 08:39:21 -0500

    Took out testing stuff in loop_pdbs.sh and added comments

--- Rosetta/scripts/loop_pdbs.sh
+++ Rosetta/scripts/loop_pdbs.sh
@@ -9,6 +9,9 @@ this_server=$6
 
 cd $inputpath
 
+#search for appropriate files based on filepattern
+#if filepattern is none searches for directories instead
+
 if [ "none" = "$filepattern" ];
 then
 	second_loop=1
@@ -18,6 +21,7 @@ else
 	ls ''$filepattern'' > files_list.txt
 fi
 
+#determines begin and end indices based on how many files there are and how many servers there are
 n_files=$( wc -l files_list.txt | awk '{print $1}')
 
 n_files_in_group=$(( $n_files / $n_servers + 1 ))
@@ -25,20 +29,25 @@ end_index=$(( $n_files_in_group * $this_server ))
 this_server=$(( $this_server - 1 ))
 begin_index=$(( $n_files_in_group * $this_server))
 
+#sets counters
 counter=0
 
 filecounter=0
 
+#loops thru file of filenames or dir names
 while read item
 do
 	
         filecounter=$(( $filecounter + 1 ))
 
+	#if filecounter is between beginning and end indices
         if [ $filecounter -gt $begin_index ] && [ $filecounter -le $end_index ];
         then
 
+		#set pdb_id
                 pdb_id=${item:0:4}
 
+		#if looping thru dirs then loop thru files inside
 		if [[ $second_loop -eq "1" ]]
 		then
 			cd $inputpath'/'$item
@@ -56,11 +65,9 @@ do
 	
 		     counter=$((counter+1))
 		     eval nohup "/home/arubenstein/CADRES/DecoyDiscrimination/Rosetta/scripts/$script" $inputpath $item $outpath'/'$pdb_id $pdb_id &
-		     if (( $counter % 5 == 0 )); 
+		     if (( $counter % 50 == 0 )); 
 			then 
 			wait
-			sleep 5
-			exit
 		     fi
 		fi
 	fi
