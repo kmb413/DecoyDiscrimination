3b1e042294d7a347d815d788658daa2d8afc01c0
Aliza Rubenstein <aliza.rubenstein@gmail.com>
2016-02-22 11:33:41 -0500

    Made several small changes: 1. fixed scripts to use divided natives folder 2. changed max_iter to 2000 (from 200) 3. added -nblist_autoupdate to min scripts

--- Rosetta/scripts/min_cst.sh
+++ Rosetta/scripts/min_cst.sh
@@ -11,10 +11,14 @@ strip_pdb="${pdb%.*}"
 
 const=$path'/'$strip_pdb"_sc.cst"
 
+set_id=$(expr match "$path" '.*\(set.\)')
+
+native_path=~/'CADRES/DecoyDiscrimination/Natives/natives.'$set_id'/'$pdb_id'.pdb'
+
 mkdir -p $outpath
 cd $outpath
 	
 grep -sq "reported success" $strip_pdb'_relax.log'
 if [ $? -gt 0 ]; then
-	~/Rosetta/main/source/bin/rosetta_scripts.static.linuxgccrelease -database ~/Rosetta/main/database -ex1 -ex2 -extrachi_cutoff 1 -use_input_sc -s $path'/'$pdb -parser:protocol ~/CADRES/DecoyDiscrimination/Rosetta/xml/min_cst.xml -constraints:cst_file $const -score:weights talaris2014_cst -in:file:native ~/CADRES/DecoyDiscrimination/Natives'/'$pdb_id'.pdb'  > $strip_pdb'_relax.log'
+	~/Rosetta/main/source/bin/rosetta_scripts.static.linuxgccrelease -database ~/Rosetta/main/database -ex1 -ex2 -extrachi_cutoff 1 -use_input_sc -s $path'/'$pdb -parser:protocol ~/CADRES/DecoyDiscrimination/Rosetta/xml/min_cst.xml -constraints:cst_file $const -score:weights talaris2014_cst -in:file:native $native_path  -nblist_autoupdate > $strip_pdb'_relax.log'
 fi 
